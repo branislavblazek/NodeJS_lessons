3de9cff651419bf754d8b6d06ff5c9b9c9e97ed8
Branislav Blazek <blazek@pap.sk>
2021-07-11 19:03:45 +0200

    mvc

--- lesson_6/app.js
+++ lesson_6/app.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
-// registre view engine
-app.set('view engine', 'ejs');
+// connect to MongoDB
+const dbURI = 'mongodb+srv://branislav:taruska@nodejstest.bw2ww.mongodb.net/node?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log('connected to DB!');
+        app.listen(3000);
+        console.log('listening on port 3000!');
+    })
+    .catch(err => console.log(err));
 
-// listen for request
-app.listen(3000);
+// register view engine
+app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+// routes
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Isiel som do Tatier', snippet: 'Videl som tam vela turistov' },
-        { title: 'Na biku do Bytce', snippet: 'Vodici su dost nepozorny' },
-        { title: 'Nedavajte psovi jest neskoro vecer', snippet: 'Ostane vam viac jedla' },
-    ];
-
-    res.render('index', { title: 'Home', blogs })
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // 404
 app.use((req, res) => {
